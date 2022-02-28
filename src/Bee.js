12a031b7bb45d3430e55602c030e8e668473d731
Kamran Masood <kam1088@gmail.com>
2022-02-28 11:19:36 -0800

    completed ES6 Bee.js class

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,18 @@
-class Bee {
-  // TODO..
-};
+// extend Grub superclass
+
+// age set to 5
+// color set to 'yellow'
+// job set to 'Keep on growing'
+// food (inherited from Grub)
+// eat (inherited from Grub)
+
+class Bee extends Grub {
+
+  constructor() {
+    super();
+    this.age = 5;
+    this.color = 'yellow';
+    this.job = 'Keep on growing';
+  }
+
+}
