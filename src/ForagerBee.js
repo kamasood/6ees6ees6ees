779d6b85e390c11d197439cefb15a86ba3b7d844
Kamran Masood <kam1088@gmail.com>
2022-02-28 11:29:24 -0800

    completed ES6 ForagerBee.js class

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,28 @@
-class ForagerBee {
-  // TODO..
-};
+// extend Bee superclass
+
+// age set to 10
+// job set to 'find pollen'
+// canFly set to true
+// treasureChest set to []
+// color (inherited from Bee) set to 'yellow'
+// food (inherited from Grub)
+// eat (inherited from Grub)
+
+// forage method, add input (treasure) to treasureChest
+
+class ForagerBee extends Bee {
+
+  constructor() {
+    super();
+    this.age = 10;
+    this.job = 'find pollen';
+    this.canFly = true;
+    this.treasureChest = [];
+    this.color = 'yellow';
+  }
+
+  forage(treasure) {
+    this.treasureChest.push(treasure);
+  }
+
+}
