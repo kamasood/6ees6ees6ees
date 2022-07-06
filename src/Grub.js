a7c8a27b280625287c1a504abe26e4e18fbb4da6
Kamran Masood <kam1088@gmail.com>
2022-07-06 14:11:36 -0700

    cleared for review work

--- src/Grub.js
+++ src/Grub.js
@@ -1,18 +1,2 @@
-// age set to 0
-// color set to 'pink'
-// food set to 'jelly'
-// eat method, returns 'Mmmmmmmm jelly'
-
 class Grub {
-
-  constructor() {
-    this.age = 0;
-    this.color = 'pink';
-    this.food = 'jelly';
-  }
-
-  eat() {
-    return 'Mmmmmmmmm jelly';
-  }
-
 }
